010858cd72dd581a8a02913139eaaafad9e376db
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-08 14:29:07 +0200

    Dichiarata la funzione ES 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function letterA (arr, lettera){
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i].split(``)[0] === lettera){
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
